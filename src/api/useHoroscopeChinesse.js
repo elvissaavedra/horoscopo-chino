5ec809228c4904103758d5c1151f4c86d2f7b94e
Mauro Rivera <mauro.rivera@glr.pe>
2021-01-20 16:42:32 -0500

    Fetched items

--- src/api/useHoroscopeChinesse.js
+++ src/api/useHoroscopeChinesse.js
@@ -5,7 +5,7 @@ import fetch from "./fetch";
 function useHoroscopeChinesse() {
     const { data, error } = useSWR("https://qacronosservices.glr.pe/api/spotlight?site_id=larepublica&_id=600865a8b511317c0f5b7cb4&no-api-cache=1&no-cache=1&status=1", fetch);
     return {
-        horoscopeChinesse: data,
+        horoscopeChinesse: data?.data?.spotlight?.data,
         isLoading: !error && !data,
         isError: error,
     };
