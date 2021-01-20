cd748f72ba329c54f1fc6a72da821981d091a1e1
silvanarl <ramossilvana495@gmail.com>
2021-01-20 18:34:42 -0500

    eliminamos p para enlaces en parrafo de info

--- src/components/Horoscopo/HoroscopoInfo.js
+++ src/components/Horoscopo/HoroscopoInfo.js
@@ -1,26 +1,42 @@
 import React from "react";
 
 const HoroscopoInfo = () => (
-    <section className="horoscopo-container">
-        <h2 className="bodyTitle">¿Qué es el horóscopo Chino?</h2>
-        <div className="bodyText">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt inventore consectetur dolore nulla non enim quos, quibusdam laudantium placeat totam aliquid, eos ipsa aspernatur ut sed neque et laboriosam maxime.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae laudantium expedita eligendi exercitationem repellat dolore quas suscipit et ratione velit, eaque, dignissimos explicabo iste earum deserunt assumenda placeat accusamus quibusdam! Aut, non qui. Accusantium ullam aut pariatur quasi officiis beatae dicta eaque quaerat consectetur earum, harum commodi esse incidunt distinctio.</p>
-            <p>Los signos del horóscopo chino son: 
-                <p className="wrapper-anchor"> <a href="#">Rata</a>, </p> 
-                <p className="wrapper-anchor"><a href="#">Búfalo</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Tigre</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Conejo</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Dragón</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Serpiente</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Caballo</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Cabra</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Mono</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Gallo</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Perro</a>, </p>
-                <p className="wrapper-anchor"><a href="#">Cerdo</a> </p>adipisicing elit. Eum nam quibusdam veritatis deserunt, ipsum reiciendis!</p>
-        </div>
-    </section>
+  <section className="horoscopo-container">
+    <h2 className="bodyTitle">¿Qué es el horóscopo Chino?</h2>
+    <div className="bodyText">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        inventore consectetur dolore nulla non enim quos, quibusdam laudantium
+        placeat totam aliquid, eos ipsa aspernatur ut sed neque et laboriosam
+        maxime.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        laudantium expedita eligendi exercitationem repellat dolore quas
+        suscipit et ratione velit, eaque, dignissimos explicabo iste earum
+        deserunt assumenda placeat accusamus quibusdam! Aut, non qui.
+        Accusantium ullam aut pariatur quasi officiis beatae dicta eaque quaerat
+        consectetur earum, harum commodi esse incidunt distinctio.
+      </p>
+      <p>
+        Los signos del horóscopo chino son:
+        {" "} <a className="sign-link" href="#">Rata</a>, {" "}
+        <a className="sign-link" href="#">Búfalo</a>, {" "}
+        <a className="sign-link" href="#">Tigre</a>, {" "}
+        <a className="sign-link" href="#">Conejo</a>, {" "}
+        <a className="sign-link" href="#">Dragón</a>, {" "}
+        <a className="sign-link" href="#">Serpiente</a>, {" "}
+        <a className="sign-link" href="#">Caballo</a>, {" "}
+        <a className="sign-link" href="#">Cabra</a>, {" "}
+        <a className="sign-link" href="#">Mono</a>, {" "}
+        <a className="sign-link" href="#">Gallo</a>, {" "}
+        <a className="sign-link" href="#">Perro</a>, {" "}
+        <a className="sign-link" href="#">Cerdo</a> {" "}
+        adipisicing elit. Eum nam quibusdam veritatis deserunt, ipsum
+        reiciendis!
+      </p>
+    </div>
+  </section>
 );
 
 export default HoroscopoInfo;
