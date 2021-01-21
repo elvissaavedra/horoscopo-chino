ecf0ebbd7af9a6640870dfaf0103606026855d47
Mauro Rivera <mauro.rivera@glr.pe>
2021-01-21 15:29:46 -0500

    Added fetching and merging items

--- src/api/useHoroscopeChinesse.js
+++ src/api/useHoroscopeChinesse.js
@@ -1,17 +1,14 @@
-import useSWR from 'swr';
+import useSWR from "swr";
 
-import fetch from './fetch';
+import fetch from "./fetch";
 
 function useHoroscopeChinesse() {
-  const { data, error } = useSWR(
-    'https://qacronosservices.glr.pe/api/spotlight?site_id=larepublica&_id=600865a8b511317c0f5b7cb4&no-api-cache=1&no-cache=1&status=1',
-    fetch
-  );
-  return {
-    horoscopeChinesse: data?.data?.spotlight?.data,
-    isLoading: !error && !data,
-    isError: error,
-  };
+    const { data, error } = useSWR("https://qacronosservices.glr.pe/api/spotlight?site_id=larepublica&_id=600863edae8c7e50bd428c64&no-api-cache=1&no-cache=1&status=1", fetch);
+    return {
+        horoscopeChinesse: data?.data?.spotlight?.data,
+        isLoading: !error && !data,
+        isError: error,
+    };
 }
 
 export default useHoroscopeChinesse;
