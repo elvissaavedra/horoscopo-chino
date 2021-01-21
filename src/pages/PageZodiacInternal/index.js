84540d1d3c4d2daee67343d6fd7dab1b54ea6ddd
silvanarl <ramossilvana495@gmail.com>
2021-01-21 10:50:15 -0500

    se agrego contenedor padre y form

--- src/pages/PageZodiacInternal/index.js
+++ src/pages/PageZodiacInternal/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Main } from './Main';
 import { Header } from './Header';
+import { SearchForm } from '../PageZodiac/SearchForm'
 
 export const PageZodiacInternal = () => {
   return (
-    <>
+    <div className="container">
       <Header />
       <Main />
-    </>
+      <SearchForm />
+    </div>
   );
 };
