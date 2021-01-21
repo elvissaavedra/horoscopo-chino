0f2aaa3bf0dc73f26e74d453505b97e10be0b6dd
Elvis Saavedra <elvis.saavedra.german@gmail.com>
2021-01-21 11:01:39 -0500

    no message

--- src/pages/PageZodiacInternal/index.js
+++ src/pages/PageZodiacInternal/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { Main } from './Main';
 import { Header } from './Header';
+import { SearchForm } from '../PageZodiac/SearchForm';
+import { Button } from './Button';
 
 export const PageZodiacInternal = () => {
   return (
     <>
       <Header />
       <Main />
+      <SearchForm />
+      <Button />
     </>
   );
 };
