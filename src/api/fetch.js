3d4aa11f3f680a720c19c6d0e6b00cb9ac9a437e
Mauro Rivera <mauro.rivera@glr.pe>
2021-01-25 12:27:12 -0500

    Added environment values

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -2,7 +2,7 @@ export default async (...args) => {
     const options = new Request(...args, {
         headers: new Headers({
             "Content-Type": "application/json",
-            token_id: process.env.TOKEN_ID,
+            token_id: process.env.REACT_APP_TOKEN_ID,
         }),
     });
 
