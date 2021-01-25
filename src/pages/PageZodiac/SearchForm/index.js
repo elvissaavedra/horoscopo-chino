13f1706ad9c7ff751a8885e62127d6fce7bdc304
Elvis Saavedra <elvis.saavedra.german@gmail.com>
2021-01-25 07:45:19 -0500

    modifacion de formulario

--- src/pages/PageZodiac/SearchForm/index.js
+++ src/pages/PageZodiac/SearchForm/index.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import moment from 'moment';
 import 'moment-lunar';
 
+import { filter, find, map, pipe, prop } from 'ramda';
+
+import { ArrayHoroscope } from '../../../shared/constant/ConstantHoroscope';
+
 // import DatePicker from 'react-date-picker';
 
 import './style.css';
@@ -9,16 +13,29 @@ import './style.css';
 let currentDay = moment().format('YYYY-MM-DD');
 
 const SearchForm = ({ data }) => {
-  // const searchSign = (evt) => {
-  //   evt.preventDefault();
-  //   let moonYear = Number(moment(evt.target.elements.date.value).lunar().format('YYYY'));
-  //   console.log(
-  //     data.find((sign) => {
-  //       return (moonYear - sign.year) % 12 === 0;
-  //     })
-  //   );
-  // };
-  const [value, onChange] = useState(new Date());
+  const [inputDate, setInputDate] = useState(currentDay);
+
+  const handleInputChange = (e) => {
+    setInputDate(e.target.value);
+  };
+
+  const searchSign = (evt) => {
+    evt.preventDefault();
+
+    // console.log(inputDate.substr(0, 4));
+
+    let searchS = pipe(
+      find((x) => x.yearsCelebrated === 2021),
+      map((x) => ArrayHoroscope)
+    );
+    console.log(searchS);
+    // let moonYear = Number(moment(evt.target.elements.date.value).lunar().format('YYYY'));
+    // console.log(
+    //   data.find((sign) => {
+    //     return (moonYear - sign.year) % 12 === 0;
+    //   })
+    // );
+  };
 
   return (
     <div className="searchForm">
@@ -30,11 +47,12 @@ const SearchForm = ({ data }) => {
         <form onSubmit={searchSign} className="searchForm__form">
           {/* <DatePicker maxDate={new Date()} className="date" name="date" onChange={onChange} value={value} /> */}
           <input
+            name="input_zodiac"
             type="date"
             className="searchForm__inputDate"
             min="1900-01-01"
             max="2031-31-12"
-            onChange={onChange}
+            onChange={handleInputChange}
             defaultValue={currentDay}
           />
           <button type="submit">BUSCAR</button>
