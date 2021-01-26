4ad3dc858de3533a97ca5a55dc5c09768a44d73e
Mauro Rivera <mauro.rivera@glr.pe>
2021-01-26 12:42:46 -0500

    Added env

--- src/api/useHoroscopeChinesse.js
+++ src/api/useHoroscopeChinesse.js
@@ -1,30 +1,27 @@
-import useSWR from 'swr';
-import { ArrayHoroscope } from '../shared/constant/ConstantHoroscope';
+import useSWR from "swr";
+import { ArrayHoroscope } from "../shared/constant/ConstantHoroscope";
 
-import fetch from './fetch';
+import fetch from "./fetch";
 
 function useHoroscopeChinesse() {
-  const { data, error } = useSWR(
-    'https://qacronosservices.glr.pe/api/spotlight?site_id=larepublica&_id=600863edae8c7e50bd428c64&no-api-cache=1&no-cache=1&status=1',
-    fetch
-  );
-  return {
-    horoscopeChinesse: data?.data?.spotlight?.data
-      ?.map((sign) =>
-        sign?.data?.map((zodiacSign) => ({
-          ...zodiacSign?.fields?.reduce((prev, current) => {
-            prev[current.name] = current.value;
-            return prev;
-          }, {}),
-          ...ArrayHoroscope.find((sign) => zodiacSign.fields.find((item) => item.name === 'url').value === sign.url),
-        }))
-      )[0]
-      .sort((prev, curr) => {
-        return prev.year - curr.year;
-      }),
-    isLoading: !error && !data,
-    isError: error,
-  };
+    const { data, error } = useSWR(`${process.env.REACT_APP_GRAPHQL_ENDPOINT}?site_id=larepublica&_id=${process.env.REACT_APP_SPOTLIGHT_ID}&status=1&cache=1440`, fetch);
+    return {
+        horoscopeChinesse: data?.data?.spotlight?.data
+            ?.map((sign) =>
+                sign?.data?.map((zodiacSign) => ({
+                    ...zodiacSign?.fields?.reduce((prev, current) => {
+                        prev[current.name] = current.value;
+                        return prev;
+                    }, {}),
+                    ...ArrayHoroscope.find((sign) => zodiacSign.fields.find((item) => item.name === "url").value === sign.url),
+                }))
+            )[0]
+            .sort((prev, curr) => {
+                return prev.year - curr.year;
+            }),
+        isLoading: !error && !data,
+        isError: error,
+    };
 }
 
 export default useHoroscopeChinesse;
