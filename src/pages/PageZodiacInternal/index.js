50dfc314d98c56847b75cb8c970526bd46dcb367
Mauro Rivera <mauro.rivera@glr.pe>
2021-01-26 11:23:22 -0500

    Finished merhing info

--- src/pages/PageZodiacInternal/index.js
+++ src/pages/PageZodiacInternal/index.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import { SearchForm } from '../PageZodiac/SearchForm';
-import { Main } from './Main';
-import { Header } from './Header';
-import useHoroscopeChinesse from '../../api/useHoroscopeChinesse';
-import { Loading } from '../../components/Loading/index';
-import { Button } from '../../shared/components/Button';
+import { SearchForm } from "../PageZodiac/SearchForm";
+import { Main } from "./Main";
+import { Header } from "./Header";
+import useHoroscopeChinesse from "../../api/useHoroscopeChinesse";
+import { Loading } from "../../components/Loading/index";
 
 export const PageZodiacInternal = () => {
-  const { sign } = useParams();
-  const { horoscopeChinesse, isLoading } = useHoroscopeChinesse();
+    const { sign } = useParams();
+    const { horoscopeChinesse, isLoading } = useHoroscopeChinesse();
 
-  if (isLoading) return <Loading />;
+    if (isLoading) return <Loading />;
 
-  return (
-    <div className="container">
-      <Header data={horoscopeChinesse} sign={sign} />
-      <Main data={horoscopeChinesse} sign={sign} />
-      <SearchForm />
-      <Button />
-    </div>
-  );
+    return (
+        <div className="container">
+            <Header data={horoscopeChinesse} sign={sign} />
+            <Main data={horoscopeChinesse} sign={sign} />
+            <SearchForm data={horoscopeChinesse} />
+        </div>
+    );
 };
